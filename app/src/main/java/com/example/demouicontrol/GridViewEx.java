22bc15fa78eee3b26c6a5b7923ab7db5d02ce24c
cutler6829 <asplion6829@gmail.com>
2018-06-11 16:20:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demouicontrol/GridViewEx.java
@@ -0,0 +1,14 @@
+package com.example.demouicontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridViewEx extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridview_ex);
+
+    }
+}
---
+++ app/src/main/java/com/example/demouicontrol/ImageAdapter.java
@@ -0,0 +1,50 @@
+package com.example.demouicontrol;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    int[] imageResIds = {R.drawable.note,
+            R.drawable.photos,
+            R.drawable.contacts,
+            R.drawable.calculator,
+            R.drawable.backup,
+            R.drawable.music,
+            R.drawable.browser};
+
+    Context mCtx;
+
+    ImageAdapter(Context context){
+        mCtx = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imageResIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mCtx);
+        imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 250));
+        imageView.setImageResource(imageResIds[position]);
+        imageView.setBackgroundResource(R.drawable.rounded_rect);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        return imageView;
+    }
+}
